056adea4944608c0f6da44c5ae5f85fa1f1bfd17
Jan Owiesniak <jowiesniak@gmail.com>
2013-04-19 10:25:56 +0200

    Remove deprecated attr_accessible call.

--- lib/i18n/backend/active_record/translation.rb
+++ lib/i18n/backend/active_record/translation.rb
@@ -50,7 +50,6 @@ module I18n
         FALSY_CHAR = "\002"
 
         self.table_name = 'translations'
-        attr_accessible :locale, :key, :value
 
         serialize :value
         serialize :interpolations, Array
